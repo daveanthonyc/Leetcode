ab953bace26d51add990e56d40c8640d4a081a5e
daveanthonyc <daveanthonyc@gmail.com>
2024-03-28 01:14:23 +1100

    update

---
+++ src/Arrays and Hashing/2 medium/topKFrequentElements/topKFrequentElements.ts
@@ -0,0 +1,12 @@
+//Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+//Input: nums = [1,1,1,2,2,3], k = 2
+//Output: [1,2]
+// 1: 3
+// 2: 2
+// 3: 1
+// determine order of which highest
+// all unique numbers in array, sorted in most frequent to least -> return slice(0,k)
+
+export function topKFrequent(nums: number[], k: number): number[] {
+
+}
