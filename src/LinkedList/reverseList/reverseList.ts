64f492f8a008d6d15be8ee1dc531598942afa26d
daveanthonyc <daveanthonyc@gmail.com>
2024-03-31 02:23:43 +1100

    reverse linkedList

---
+++ src/LinkedList/reverseList/reverseList.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+type ListNode = {
+    val: number,
+    next: ListNode | null
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null;
+    let curr = head;
+
+    while (curr) {
+        const savedNext = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = savedNext;
+    }
+
+    return prev;
+};
